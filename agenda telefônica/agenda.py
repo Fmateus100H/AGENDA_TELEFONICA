68effc5cb3e5bf3d79b216d1eb5942c515f57593
Francisco Mateus <91853688+Fmateus100H@users.noreply.github.com>
2021-11-24 21:40:12 -0300

    agenda v1.0
    No momento a lista de telefones só fica salvo enquanto é executado, mas já estou no caminho de dar um UP nisso

---
+++ agenda telefônica/agenda.py
@@ -0,0 +1,98 @@
+class Contato:
+  nome: str
+  telefone: int
+  idade: int
+
+def menu():
+  
+  menu = '''
+=============
+    MENU
+=============
+
+Digite o número da opção que deseja realizar com os contatos:
+(1) Cadastrar novo contato
+(2) Localizar contato
+(3) Alterar contato
+(4) Excluir contato
+(5) Listar seus contatos
+'''
+  
+  print(menu)
+  N = int(input())
+  return N
+
+
+def opcoes(N, lista):
+  if N == 1:
+    return contatos(lista)
+  if N == 2:
+    localizar(lista)
+    return lista
+  if N == 3:
+    return alterar(lista)
+  if N == 4:
+    return excluir(lista)
+  if N == 5:
+    mostrar(lista)
+    return lista
+
+
+def contatos(lista) -> list:
+  if len(lista) <= 10:
+    ctt = Contato()
+    ctt.nome = input('NOME\n')
+    ctt.telefone = int(input('TELEFONE\n'))
+    ctt.idade = int(input('IDADE\n'))
+    lista.append(ctt)
+  return lista
+
+
+def localizar(lista):
+  L = input('Digite o nome do contato\n')
+  tam = len(lista)
+  for i in range(tam):
+    if L == lista[i].nome:
+      print(f'\nTelefone: {lista[i].telefone}\nIdade: {lista[i].idade}')
+
+
+def alterar(lista):
+  L = input('\nDigite o nome do contato\n')
+  D = int(input('\nQual informação deseja alterar?\n(1) Nome\n(2) Telefone\n(3) Idade\n'))
+  tam = len(lista)
+  for i in range(tam):
+    if L == lista[i].nome:
+      if D == 1:
+        lista[i].nome = input('\nDigite o novo nome\n')
+      elif D == 2:
+        lista[i].telefone = int(input('\nDigite o novo telefone\n'))
+      elif D == 3:
+        lista[i].idade = int(input('\nDigite a nova idade\n'))
+    break
+  return lista
+
+
+def excluir(lista):
+  N = input('\nInforme o nome do contato que deseja excluir\n')
+  tam = len(lista)
+  for i in range(tam):
+    if lista[i].nome == N:
+      del lista[i]
+      break
+  return lista
+
+
+def mostrar(lista: list) -> None:
+  tam = len(lista)
+  for i in range(tam):
+    print('NOME:', lista[i].nome)
+    print('TELEFONE:', lista[i].telefone)
+    print('IDADE:', lista[i].idade)
+    print()
+
+   
+tamanho = 1
+lista_contatos = []
+while True:
+  N = menu()
+  lista = opcoes(N, lista_contatos)
