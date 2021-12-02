d7d0a94ae44b643376a4fb2b8b84b5427b08f529
Francisco Mateus <91853688+Fmateus100H@users.noreply.github.com>
2021-12-02 19:17:06 -0300

    Update agenda.py

--- agenda telefônica/agenda.py
+++ agenda telefônica/agenda.py
@@ -1,7 +1,12 @@
+nome_arquivo = 'agenda.txt'
+arquivo = open(nome_arquivo, 'a')
+arquivo.close()
+
+
 class Contato:
   nome: str
   telefone: int
-  idade: int
+  adicional: str
 
 def menu():
   
@@ -25,14 +30,14 @@ Digite o número da opção que deseja realizar com os contatos:
 def opcoes(N, lista):
   if N == 1:
     return contatos(lista)
-  if N == 2:
+  elif N == 2:
     localizar(lista)
     return lista
-  if N == 3:
+  elif N == 3:
     return alterar(lista)
-  if N == 4:
+  elif N == 4:
     return excluir(lista)
-  if N == 5:
+  elif N == 5:
     mostrar(lista)
     return lista
 
@@ -41,8 +46,9 @@ def contatos(lista: list) -> list:
     ctt = Contato()
     ctt.nome = input('NOME\n')
     ctt.telefone = int(input('TELEFONE\n'))
-    ctt.idade = int(input('IDADE\n'))
+    ctt.adicional = input('INFORMAÇÃO ADICIONAL\n')
     lista.append(ctt)
+    gravar_arquivo(nome_arquivo, lista)
     return lista
 
 
@@ -55,7 +61,7 @@ def localizar(lista: list) -> None:
 
 def alterar(lista: list) -> list:
   L = input('\nDigite o nome do contato\n')
-  D = int(input('\nQual informação deseja alterar?\n(1) Nome\n(2) Telefone\n(3) Idade\n'))
+  D = int(input('\nQual informação deseja alterar?\n(1) Nome\n(2) Telefone\n(3) Inf. adicional\n'))
   for i in range(len(lista)):
     if L == lista[i].nome:
       if D == 1:
@@ -63,8 +69,9 @@ def alterar(lista: list) -> list:
       elif D == 2:
         lista[i].telefone = int(input('\nDigite o novo telefone\n'))
       elif D == 3:
-        lista[i].idade = int(input('\nDigite a nova idade\n'))
+        lista[i].adicional = input('\nDigite a nova informação\n')
       break
+  gravar_arquivo(nome_arquivo, lista)
   return lista
 
 
@@ -74,6 +81,7 @@ def excluir(lista: list) -> list:
     if lista[i].nome == N:
       del lista[i]
       break
+  gravar_arquivo(nome_arquivo, lista)
   return lista
 
 
@@ -81,11 +89,33 @@ def mostrar(lista: list) -> None:
   for i in range(len(lista)):
     print('NOME:', lista[i].nome)
     print('TELEFONE:', lista[i].telefone)
-    print('IDADE:', lista[i].idade)
+    print('INF. ADICIONAL:', lista[i].adicional)
     print()
 
+def ler_arquivo(nome_arquivo: str) -> list:
+  arquivo = open(nome_arquivo, 'r')
+  lista = []
+  for linha in arquivo.readlines():
+    C = Contato()
+    linha = linha.replace('\n', '')
+    posicao = linha.split('##')
+    C.nome = linha[0]
+    C.telefone = linha[1]
+    C.adicional = linha[2]
+    lista.append(C)
+  arquivo.close()
+  return lista
 
-lista_contatos = []
+
+def gravar_arquivo(nome_arquivo: str, lista: list):
+  arquivo = open(nome_arquivo, 'w')
+  for i in range(len(lista_contatos)):
+    L = f'{lista[i].nome}##{lista[i].telefone}##{lista[i].adicional}\n'
+    arquivo.write(L)
+  arquivo.close()
+  
+
+lista_contatos = ler_arquivo(nome_arquivo)
 while True:
   N = menu()
   lista = opcoes(N, lista_contatos)
